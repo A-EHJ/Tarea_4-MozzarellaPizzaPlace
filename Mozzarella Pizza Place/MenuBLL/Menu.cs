8049127f06ce56195573618645c57bd125713a5b
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-23 11:33:22 -0400

    Agregando Menu(BLL)

---
+++ Mozzarella Pizza Place/MenuBLL/Menu.cs
@@ -0,0 +1,13 @@
+﻿
+using Mozzarella_Pizza_Place.Models;
+
+namespace Mozzarella_Pizza_Place.MenuBLL
+{
+    public class Menu
+    {
+      
+        public List<Pizzas> Pizzas { get; set; } = new List<Pizzas>();
+        public void Agregar(Pizzas pizza) => Pizzas.Add(pizza);
+        public Pizzas? ObtenerPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
