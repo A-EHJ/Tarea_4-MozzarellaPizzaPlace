db144bb1c6ad19784116ade762afa7e4fd84c10c
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-24 14:07:11 -0400

    Cambiando nombre a una funcion para obtener una pizza

--- Mozzarella Pizza Place/MenuBLL/Menu.cs
+++ Mozzarella Pizza Place/MenuBLL/Menu.cs
@@ -8,6 +8,6 @@ namespace Mozzarella_Pizza_Place.MenuBLL
       
         public List<Pizzas> Pizzas { get; set; } = new List<Pizzas>();
         public void Agregar(Pizzas pizza) => Pizzas.Add(pizza);
-        public Pizzas? ObtenerPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.PizzaId == id);
+        public Pizzas? GetPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.PizzaId == id);
     }
 }
