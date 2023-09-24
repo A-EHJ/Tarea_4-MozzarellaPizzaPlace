7a2b126235a7b1090c853b829c8ff787096134d4
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-24 14:12:07 -0400

    Creando la clase Estado

---
+++ Mozzarella Pizza Place/Models/Estado.cs
@@ -0,0 +1,13 @@
+﻿using Mozzarella_Pizza_Place.MenuBLL;
+
+namespace Mozzarella_Pizza_Place.Models
+{
+    public class Estado
+    {
+        public Menu Menu { get; } = new Menu();
+        public CarritoDeCompra CarritoDeCompra { get; } = new CarritoDeCompra();
+        public UI UI { get; set; } = new UI();
+
+        public decimal PrecioTotal => CarritoDeCompra.Ordenes.Sum(id => Menu.GetPizza(id)!.Price);
+    }
+}
