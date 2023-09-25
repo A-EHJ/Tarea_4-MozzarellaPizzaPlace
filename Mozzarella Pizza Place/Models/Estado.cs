a943aa3e500336179fc67dc151fd5ecbd10be9bb
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-24 20:08:29 -0400

    Quitando Espacios

--- Mozzarella Pizza Place/Models/Estado.cs
+++ Mozzarella Pizza Place/Models/Estado.cs
@@ -7,7 +7,6 @@ namespace Mozzarella_Pizza_Place.Models
         public Menu Menu { get; } = new Menu();
         public CarritoDeCompra CarritoDeCompra { get; } = new CarritoDeCompra();
         public UI UI { get; set; } = new UI();
-
-        public decimal PrecioTotal => CarritoDeCompra.Ordenes.Sum(id => Menu.GetPizza(id)!.Price);
+        public decimal PrecioTotal => CarritoDeCompra.Ordenes.Sum(id => Menu.GetPizza(id)!.Precio);
     }
 }
