7f4e4495fd5a34519c1ae7954ef0155a5ce3bd6f
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-24 20:08:51 -0400

    Agregando funcnion ara remover pizza de las ordenes

--- Mozzarella Pizza Place/Models/CarritoDeCompra.cs
+++ Mozzarella Pizza Place/Models/CarritoDeCompra.cs
@@ -6,5 +6,6 @@
         public List<int> Ordenes { get; set; } = new List<int>();
         public bool EstaPago { get; set; }
         public void AgregarALasOrdenes(int pizzaId) => Ordenes.Add(pizzaId);
+        public void RemoverLaPosicion(int pos) => Ordenes.RemoveAt(pos);
     }
 }
