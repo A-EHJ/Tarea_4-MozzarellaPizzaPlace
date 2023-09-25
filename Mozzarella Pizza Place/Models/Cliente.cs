89905c6f105896f3e162dc60487a3dc7966a7d5f
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-24 20:05:55 -0400

    Agregando Validaciones y mas campos al Cliente

--- Mozzarella Pizza Place/Models/Cliente.cs
+++ Mozzarella Pizza Place/Models/Cliente.cs
@@ -1,10 +1,30 @@
-﻿namespace Mozzarella_Pizza_Place.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mozzarella_Pizza_Place.Models
 {
     public class Cliente
     {
-        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "El nombre solo debe contener letras y espacios.
         public string Nombre { get; set; }
-        public int Direccion { get; set; }
+
+        [Required(ErrorMessage = "La dirección es obligatoria.")]
+        [MaxLength(100, ErrorMessage = "La dirección no puede tener más de 100 caracteres.")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "La ciudad es obligatoria.")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "La ciudad solo debe contener letras y espacios.")]
         public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El número de tarjeta no es válido.")]
+        [CreditCard(ErrorMessage = "El número de tarjeta no es válido.")]
+        public string NumeroTarjeta { get; set; }
+    
     }
+
 }
